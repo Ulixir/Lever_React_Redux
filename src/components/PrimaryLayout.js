8f6ba5a25b565bc535f696f4711537f3c178d5b3
zero2smart <fivewisdomzero@gmail.com>
2018-09-28 18:18:26 -0400

    First Commit

---
+++ src/components/PrimaryLayout.js
@@ -0,0 +1,19 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+import '../assets/stylesheets/components/PrimaryLayout.scss';
+import HomePage from './HomePage';
+
+const PrimaryLayout = props => {
+    return (
+        <div className="primary-layout">
+            {/* <PrimaryHeader /> */}
+            <main>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Redirect to="/" />
+            </Switch>
+            </main>
+      </div>
+    );
+}
+
+export deafult PrimaryLayout;
